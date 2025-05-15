2401c5776c3eef96fc9c3c5e9710292a90b56e31
ElbouazizifEzzahra <fatimaezzahraelbouazizi@gmail.com>
2025-05-15 09:34:39 +0100

    main pour teste la connexion

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
-import java.time.DayOfWeek;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-public class Main {
-    public static void main(String[] args) {
+        public class Main {
+            public static void main(String[] args) {
+                String url = "jdbc:mysql://localhost:3306/college";
+                String user = "root";
+                String password = "";
+                try {
+                    Connection conn = DriverManager.getConnection(url, user, password);
+                    System.out.println("Connexion réussie !");
+                    conn.close();
+                } catch (SQLException e) {
+                    System.out.println("Échec de la connexion !");
+                    e.printStackTrace();
+                }
+            }
+        }
 
-    }
-}
--- src/util/DBUtil.java
+++ src/util/DBUtil.java
@@ -8,7 +8,7 @@ public class DBUtil {
     static{
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            String url="jdbc:mysql:://localhoast:3306/college";
+            String url="jdbc:mysql://localhost:3306/college";
             conn= DriverManager.getConnection(url,"root","");
         }catch(Exception e){
             System.out.println(" erreur lors de telechargement de driver"+e);
