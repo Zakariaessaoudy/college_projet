09a27b1241461d002b9497d7ac67368406335054
ElbouazizifEzzahra <fatimaezzahraelbouazizi@gmail.com>
2025-05-15 09:15:20 +0100

    DbUtil + controllers+ dao =>  seance classe eleve vides

--- src/util/DBUtil.java
+++ src/util/DBUtil.java
@@ -1,4 +1,20 @@
 package util;
 
+import java.sql.*;
+
+
 public class DBUtil {
+    private static Connection conn;
+    static{
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            String url="jdbc:mysql:://localhoast:3306/college";
+            conn= DriverManager.getConnection(url,"root","");
+        }catch(Exception e){
+            System.out.println(" erreur lors de telechargement de driver"+e);
+        }
+    }
+    public static Connection getConnection(){
+        return conn;
+    }
 }
